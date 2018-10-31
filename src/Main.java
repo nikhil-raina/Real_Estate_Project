28edcabfc55db3118fb9b15cdd633e52b3fafcc2
Justin <jbk2042@g.rit.edu>
2018-10-30 20:15:02 -0400

    Commiting a connection to postgres, we need to have the user input their user and password right now to get on -Justin

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Main {
+    public static void main(String args[]) {
+
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/Real_Estate",
+                            "postgres", "123");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+        }
+
+}
