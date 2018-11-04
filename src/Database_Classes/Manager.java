3d4cf8f1ec4e14149be69d4991c14147d13258af
Nikhil Raina <nxr5013@g.rit.edu>
2018-11-04 01:47:44 -0500

    created the manager class with its attributes and methods

--- src/Database_Classes/Manager.java
+++ src/Database_Classes/Manager.java
@@ -1,5 +1,32 @@
 package Database_Classes;
 
-public class Manager {
-    
+public class Manager extends Person{
+
+    private int salary;
+    private int officeID;
+
+    public Manager(String firstName, String lastName, char[] taxID, char memberType, String phoneNumber,
+                   char[] zipCode, String streetAddress, char[] state, int salary, int officeID) {
+
+        super(firstName, lastName, taxID, memberType, phoneNumber, zipCode, streetAddress, state);
+
+        this.officeID = officeID;
+        this.salary = salary;
+    }
+
+    public int getOfficeID() {
+        return officeID;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setOfficeID(int officeID) {
+        this.officeID = officeID;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
