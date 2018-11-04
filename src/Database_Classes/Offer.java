71e21e809094ce704fdb261f4266a1d2ef9aad9d
Nikhil Raina <nxr5013@g.rit.edu>
2018-11-04 15:50:10 -0500

    Created the basic Offer class

--- src/Database_Classes/Offer.java
+++ src/Database_Classes/Offer.java
@@ -1,4 +1,70 @@
 package Database_Classes;
 
+
+import java.sql.Date;
+
 public class Offer {
+
+    private Property propertyID; //
+    private int offerID;
+    private Date date;
+    private char[] buyerTaxID;
+    private int offerAmount;
+    private char offerStatus;
+
+    public Offer(int propertyID, int offerID, Date date, char[] buyerTaxID, int offerAmount, char offerStatus){
+        this.buyerTaxID = buyerTaxID;
+        this.offerID = offerID;
+        this.date = date;
+        this.offerAmount = offerAmount;
+        this.offerStatus = offerStatus;
+        this.propertyID.setPropertyID(propertyID);
+    }
+
+    public void setBuyerTaxID(char[] buyerTaxID) {
+        this.buyerTaxID = buyerTaxID;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setOfferAmount(int offerAmount) {
+        this.offerAmount = offerAmount;
+    }
+
+    public void setOfferID(int offerID) {
+        this.offerID = offerID;
+    }
+
+    public void setOfferStatus(char offerStatus) {
+        this.offerStatus = offerStatus;
+    }
+
+    @Override
+    public void setPropertyID(Property propertyID) {
+        this.propertyID = propertyID;
+    }
+
+
+    public char getOfferStatus() {
+        return offerStatus;
+    }
+
+    public char[] getBuyerTaxID() {
+        return buyerTaxID;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public int getOfferAmount() {
+        return offerAmount;
+    }
+
+    public int getOfferID() {
+        return offerID;
+    }
+
 }
