600c5c7ba1408c557f74d2d97de3f8d5b01c936e
Nikhil Raina <nxr5013@g.rit.edu>
2018-11-04 18:39:42 -0500

    Created the Agent class and brought about the inheriting aspect of Person

--- src/Database_Classes/Agent.java
+++ src/Database_Classes/Agent.java
@@ -1,5 +1,38 @@
 package Database_Classes;
 
-public class Agent {
+public class Agent extends Person{
 
+    private Person person; // taxID
+    private int salary;
+    private int commission;
+    private Manager manager; // ID of Manager.
+    private Office office; // ID of the office the agent will work in
+
+
+    public Agent(char[] zipcode, String streetAddress, char[] state, String firstName, String lastName,
+                 char[] taxID, char memberType, String phoneNumber, int salary, int commission,
+                 char[] ManagerTaxID, int PrimaryOfficeID) {
+        super(zipcode, streetAddress, state, firstName, lastName, taxID, memberType, phoneNumber);
+
+        this.salary = salary;
+        this.commission = commission;
+        manager.setTaxID(ManagerTaxID);
+        office.setOfficeID(PrimaryOfficeID);
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setCommission(int commission) {
+        this.commission = commission;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getCommission() {
+        return commission;
+    }
 }
