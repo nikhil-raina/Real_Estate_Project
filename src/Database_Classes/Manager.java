a7a02df24feda3f00ab234c00451f633c694dadb
Nikhil Raina <nxr5013@g.rit.edu>
2018-11-04 18:41:19 -0500

    Removed the overriding function of setTaxID

--- src/Database_Classes/Manager.java
+++ src/Database_Classes/Manager.java
@@ -3,17 +3,16 @@ package Database_Classes;
 public class Manager extends Person{
 
     private int salary;
-//    private int officeID;
     private Person person;
     private Office office;
 
-    public Manager(String firstName, String lastName, char[] taxID, char memberType, String phoneNumber,
-                   char[] zipCode, String streetAddress, char[] state, int salary, int officeID) {
 
-        super(firstName, lastName, taxID, memberType, phoneNumber, zipCode, streetAddress, state);
+    public Manager(char[] zipcode, String streetAddress, char[] state, String firstName, String lastName,
+                   char[] taxID, char memberType, String phoneNumber, int salary, int officeID) {
 
-        person.setTaxID(taxID);
-//        office.setOfficeID(officeID);
+        super(zipcode, streetAddress, state, firstName, lastName, taxID, memberType, phoneNumber);
+        this.person.setTaxID(taxID);
+        this.office.setOfficeID(officeID);
         this.salary = salary;
     }
 
