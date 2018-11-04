1f5bc74d89ed5934c9e93c95fbac6abf80f4c160
Nikhil Raina <nxr5013@g.rit.edu>
2018-11-04 02:11:59 -0500

    Created basic model for Manager

--- src/Database_Classes/Manager.java
+++ src/Database_Classes/Manager.java
@@ -3,27 +3,22 @@ package Database_Classes;
 public class Manager extends Person{
 
     private int salary;
-    private int officeID;
+//    private int officeID;
+    private Person person;
+    private Office office;
 
     public Manager(String firstName, String lastName, char[] taxID, char memberType, String phoneNumber,
                    char[] zipCode, String streetAddress, char[] state, int salary, int officeID) {
 
         super(firstName, lastName, taxID, memberType, phoneNumber, zipCode, streetAddress, state);
 
-        this.officeID = officeID;
+        person.setTaxID(taxID);
+//        office.setOfficeID(officeID);
         this.salary = salary;
-    }
-
-    public int getOfficeID() {
-        return officeID;
     }
 
     public int getSalary() {
         return salary;
-    }
-
-    public void setOfficeID(int officeID) {
-        this.officeID = officeID;
     }
 
     public void setSalary(int salary) {
