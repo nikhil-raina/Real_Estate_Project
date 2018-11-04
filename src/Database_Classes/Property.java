8ac5555dae40c17800a91b0e6a183461541f2be7
Nikhil Raina <nxr5013@g.rit.edu>
2018-11-04 16:05:40 -0500

    Created the basic class for Property

--- src/Database_Classes/Property.java
+++ src/Database_Classes/Property.java
@@ -1,4 +1,70 @@
 package Database_Classes;
 
 public class Property {
+
+    private int propertyID;
+    private char[] agentTaxID;
+    private char[] sellerTaxID;
+    private int listPrice;
+    private String description;
+    private int area;
+
+    public Property(int propertyID, char[] agentTaxID, char[] sellerTaxID,
+                    int listPrice, String description, int area){
+
+        this.propertyID = propertyID;
+        this.agentTaxID = agentTaxID;
+        this.sellerTaxID = sellerTaxID;
+        this.area = area;
+        this.listPrice = listPrice;
+        this.description = description;
+    }
+
+    public void setPropertyID(int propertyID) {
+        this.propertyID = propertyID;
+    }
+
+    public void setAgentTaxID(char[] agentTaxID) {
+        this.agentTaxID = agentTaxID;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setListPrice(int listPrice) {
+        this.listPrice = listPrice;
+    }
+
+    public void setSellerTaxID(char[] sellerTaxID) {
+        this.sellerTaxID = sellerTaxID;
+    }
+
+    public char[] getSellerTaxID() {
+        return sellerTaxID;
+    }
+
+    public char[] getAgentTaxID() {
+        return agentTaxID;
+    }
+
+    public int getListPrice() {
+        return listPrice;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public int getPropertyID() {
+        return propertyID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
