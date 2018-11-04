7bedaa6f3837dce6835f9ea0f63dd1dc0779de45
Nikhil Raina <nxr5013@g.rit.edu>
2018-11-04 18:36:59 -0500

    Created the inherit aspect with Address.

--- src/Database_Classes/Office.java
+++ src/Database_Classes/Office.java
@@ -1,4 +1,30 @@
 package Database_Classes;
 
-public class Office {
+public class Office extends Address{
+
+    private int officeID;
+    private char[] managerID;
+
+
+    public Office(char[] zipcode, String streetAddress, char[] state, int officeID, char[] managerID) {
+        super(zipcode, streetAddress, state);
+        this.officeID = officeID;
+        this.managerID = managerID;
+    }
+
+    public void setOfficeID(int officeID) {
+        this.officeID = officeID;
+    }
+
+    public void setManagerID(char[] managerID) {
+        this.managerID = managerID;
+    }
+
+    public int getOfficeID() {
+        return officeID;
+    }
+
+    public char[] getManagerID() {
+        return managerID;
+    }
 }
