a53bcb3c1d309c22c96219c082b5dfd6e0c68006
Nikhil Raina <nxr5013@g.rit.edu>
2018-11-04 18:52:57 -0500

    Created the Client class. Inherits the Person class.

--- src/Database_Classes/Client.java
+++ src/Database_Classes/Client.java
@@ -1,4 +1,20 @@
 package Database_Classes;
 
-public class Client {
+public class Client extends Person{
+
+    private Property propertyID;
+    public Client(char[] zipcode, String streetAddress, char[] state, String firstName, String lastName,
+                  char[] taxID, char memberType, String phoneNumber, int propertyID) {
+
+        super(zipcode, streetAddress, state, firstName, lastName, taxID, memberType, phoneNumber);
+        this.propertyID.setPropertyID(propertyID);
+    }
+
+    public void setPropertyID(int propertyID) {
+        this.propertyID.setPropertyID(propertyID);
+    }
+
+    public int getPropertyID() {
+        return this.propertyID.getPropertyID();
+    }
 }
