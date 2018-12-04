9fddd185008fc95a6ddec1e7725d36544fec648c
Nikhil Raina <nxr5013@g.rit.edu>
2018-12-04 14:15:42 -0500

    Using for testing files

--- src/Backend_SQL/SQLTest.java
+++ src/Backend_SQL/SQLTest.java
@@ -11,20 +11,25 @@ class SQLTest{
         List<String> list;
         try {
             con = SQLConnection.getConnection("real_estate");
-            rs = Query_Execution.executeQuery(con, "SELECT * FROM address;");
+            rs = Query_Execution.executeQuery(con, "SELECT * FROM address");
+            ResultSetMetaData rsmd = rs.getMetaData();
+
+            int columnsNumber = rsmd.getColumnCount();
+            System.out.println(rsmd.getColumnName(1));
+            System.out.println(columnsNumber);
             while (rs.next()) {
 
                 // creating a list to add all the column values at that column.
                 // string, column header names or column position number, starting from 1, can be used in order to
                 // get the required columns.
                 list = new ArrayList<>();
-                list.add(rs.getString("streetaddress"));
-                list.add(rs.getString("city"));
-                list.add(rs.getString("state"));
+                list.add(rs.getString(1));
+                list.add(rs.getString(2));
+                list.add(rs.getString(3));
                 list.add(rs.getString(4));
                 list.add(rs.getString(5));
-
                 System.out.println(list);
+//                System.out.println(rs.getString("tablename").toUpperCase());
             }
             con.close();
         } catch (SQLException e) {
